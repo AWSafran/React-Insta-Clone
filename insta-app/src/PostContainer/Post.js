fcb2322a488f9fcf908596a1b8f1ae6f6779d72c
AWSafran <andrewsafran@gmail.com>
2019-03-28 18:47:59 -0400

    refactored post using styled components:

--- insta-app/src/PostContainer/Post.js
+++ insta-app/src/PostContainer/Post.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import './Post.css';
 
+import styled, {css} from 'styled-components';
+
+const PostHeader = styled.div`
+    display: flex;
+    height: 40px;
+    width: 77.5%;
+    margin: auto;
+    border: 1px solid #e6e6e6;
+    border-bottom: none;
+    border-radius: 2px;
+    padding: 10px;
+`;
+
+const HeaderImg = styled.img`
+    border-radius: 50%;
+    height: 30px;
+    width: auto;
+`;
+
+const HeaderUsername = styled.h2`
+    margin-left: 20px;
+    padding-top: 8px;
+    font-size: 1.6rem;
+`;
+
+const PostImg = styled.img`
+    width: 80%;
+`;
+
 const Post = props =>{
     //console.log(props);
     return(
         <div>
-            <div className="postHeader">
-                <img className="thumbnail" src={props.thumbImg} alt="User Thumbnail"/>
-                <h2>{props.user}</h2>
-            </div>
-            <img className="mainImage" src={props.postImage} alt="Post"/>
+            <PostHeader>
+                <HeaderImg src={props.thumbImg} alt="User Thumbnail"/>
+                <HeaderUsername>{props.user}</HeaderUsername>
+            </PostHeader>
+            <PostImg src={props.postImage} alt="Post"/>
         </div>
     );
 }
