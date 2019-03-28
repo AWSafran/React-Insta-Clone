a8401c2ae53149ab4334e3f6d54efeecb9a70f11
AWSafran <andrewsafran@gmail.com>
2019-03-28 15:16:34 -0400

    added styled-components dependency

--- insta-app/src/Authentication/withAuthenticate.js
+++ insta-app/src/Authentication/withAuthenticate.js
@@ -3,13 +3,13 @@ import React from 'react';
 const withAuthenticate = Comp => OtherComp=> props =>{
     console.log(localStorage.getItem('user'));
     if(localStorage.getItem('user')){
-        console.log("we have stored a user!")
+
         return <Comp 
-        searchChange={props.searchChange}
-        search={props.search}
-        searchSubmit={props.searchSubmit}
-        posts={props.posts}
-    />
+            searchChange={props.searchChange}
+            search={props.search}
+            searchSubmit={props.searchSubmit}
+            posts={props.posts}
+        />
     }
     
     return <OtherComp/>
