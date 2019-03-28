465da3c2b8c2a531b82802249daa0e5c6f9dcd7a
AWSafran <andrewsafran@gmail.com>
2019-03-28 18:15:43 -0400

    refactored searchbar using styled components

--- insta-app/src/SearchBar/SearchBar.js
+++ insta-app/src/SearchBar/SearchBar.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import '../SearchBar/SearchBar.css'
 
+import styled, {css} from 'styled-components';
+
+const Header = styled.header`
+display: flex;
+width: 95%;
+text-align: center;
+justify-content: space-between;
+height: 40px;
+margin: 10px auto;
+font-size: 4.8rem;
+`;
+
+const Form = styled.form`
+    width: 40%;
+`;
+
+const Nav = styled.nav`
+    width: 20%;    
+`;
+
+const Input = styled.input`
+    border: 1px solid #e6e6e6;
+    border-radius: 2px;
+    width: 90%;
+`;
+
 const SearchBar = props =>{
     return(
-        <header>
+        <Header>
             <div className="logo"><i className="fab fa-instagram"></i> Instagram</div>
-            <form onSubmit={props.searchSubmit}>
-                <input 
+            <Form onSubmit={props.searchSubmit}>
+                <Input 
                     type='text' 
                     value={props.search}
                     onChange={props.searchChange}
                 />
-            </form>
-            <nav> <i className="far fa-compass"></i><i className="far fa-heart"></i><i className="far fa-user"></i></nav>
-        </header>
+            </Form>
+            <Nav> <i className="far fa-compass"></i><i className="far fa-heart"></i><i className="far fa-user"></i></Nav>
+        </Header>
     )
 }
 
